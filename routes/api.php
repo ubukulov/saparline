26d013e755d116ccc765f48a8ad5591583499d82
Kairat Ubukulov <ubukulov@gmail.com>
2021-12-15 17:20:31 +0600

    fix

--- routes/api.php
+++ routes/api.php
@@ -99,16 +99,14 @@ Route::prefix('v1')->namespace('Api')->group(function () {
         Route::get('call', 'UserController@call');
         Route::get('order/take/{id}', 'UserController@orderTake');
 
-
-    });
-
-    # Lodgers Api
-    Route::group(['prefix' => 'lodger'], function(){
-        Route::get('company/{company_id}/get-cars-list', 'LodgerController@getCarsList');
-        Route::post('fix-selected-cars-for-me', 'LodgerController@fixSelectedCarsForMe');
-        Route::get('{user_id}/get-selected-cars-list', 'LodgerController@getSelectedCarsList');
-        Route::get('car-travel/{car_travel_id}/get-all-places-for-route', 'LodgerController@getAllPlacesForRoute');
-        Route::post('car-travel/{car_travel_id}/selling', 'LodgerController@ticketSelling');
-        Route::get('car-travel/{car_travel_id}/get-all-sold-places-for-route', 'LodgerController@getAllSoldPlacesForRoute');
+        # Lodgers Api
+        Route::group(['prefix' => 'lodger'], function(){
+            Route::get('company/{company_id}/get-cars-list', 'LodgerController@getCarsList');
+            Route::post('fix-selected-cars-for-me', 'LodgerController@fixSelectedCarsForMe');
+            Route::get('{user_id}/get-selected-cars-list', 'LodgerController@getSelectedCarsList');
+            Route::get('car-travel/{car_travel_id}/get-all-places-for-route', 'LodgerController@getAllPlacesForRoute');
+            Route::post('car-travel/{car_travel_id}/selling', 'LodgerController@ticketSelling');
+            Route::get('car-travel/{car_travel_id}/get-all-sold-places-for-route', 'LodgerController@getAllSoldPlacesForRoute');
+        });
     });
 });
