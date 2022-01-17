8c2b024ad76948d94628fa720faed056f1805a15
Kairat Ubukulov <ubukulov@gmail.com>
2022-01-18 05:31:12 +0600

    fix sql request

--- app/Http/Controllers/Api/CashierController.php
+++ app/Http/Controllers/Api/CashierController.php
@@ -49,7 +49,7 @@ class CashierController extends Controller {
         if (!Hash::check($password, $cashier->password)){
             return response()->json('Неверные данные',400);
         }
-		
+
 		if ($cashier->active == 0) {
 			return response()->json('Пользователь еще не активирован', 409);
 		}
@@ -58,7 +58,7 @@ class CashierController extends Controller {
 			return response()->json(['user' => new CashierResource($cashier)], 200);
 		}
 	}
-	
+
 	public function register(Request $request)
 	{
 		$rules = [
@@ -76,7 +76,7 @@ class CashierController extends Controller {
         if ($validator->fails()) {
             return response()->json($validator->errors()->first(),400,['charset'=>'utf-8'],JSON_UNESCAPED_UNICODE);
         }
-		
+
 		$data = $request->all();
 		$data['active'] = 0;
 		$data['password'] = bcrypt($data['password']);
@@ -84,7 +84,7 @@ class CashierController extends Controller {
 		if ($cashier) {
 			return response()->json('success', 200);
 		}
-		
+
 		return response()->json('Server error. Please try later', 500);
 	}
 
@@ -102,26 +102,26 @@ class CashierController extends Controller {
             ->get();
         return response()->json($company_cars);
     }
-	
+
 	public function getCities()
 	{
 		return response()->json(City::all());
 	}
-	
+
 	public function getCityStationsList($city_id)
 	{
 		$city = City::findOrFail($city_id);
 		return response()->json($city->stations);
 	}
-	
+
 	public function createTripByCashier(Request $request)
 	{
 		$data = $request->all();
 		$data['departure_time'] = Carbon::create($data['departure_time'])->format('Y-m-d H:i:s');
 		$data['destination_time'] = Carbon::create($data['destination_time'])->format('Y-m-d H:i:s');
-		
+
 		DB::beginTransaction();
-		
+
 		try {
 			$travel = Travel::join('cities as from_city','from_city.id','travel.from_city_id')
             ->join('cities as to_city','to_city.id','travel.to_city_id')
@@ -131,8 +131,8 @@ class CashierController extends Controller {
             ->where('to_station.id',$data['to_station_id'])
             ->select('travel.*')
             ->first();
-			
-			
+
+
 			$carTravel = new CarTravel();
 
 			$carTravel->from_station_id = $request['from_station_id'];
@@ -144,7 +144,7 @@ class CashierController extends Controller {
 			$carTravel->to_city_id = $data['to_city_id'];
 			$carTravel->car_id = $data['car_id'];
 			$carTravel->save();
-			
+
 			if (Station::where('id',$data['from_station_id'])->exists()){
 				$travelStation = new CarTravelStation();
 				$travelStation->car_travel_id = $carTravel->id;
@@ -153,7 +153,7 @@ class CashierController extends Controller {
 			} else{
 				return response()->json('station_id error',400,['charset'=>'utf-8'],JSON_UNESCAPED_UNICODE);
 			}
-			
+
 			if (Station::where('id',$data['to_station_id'])->exists()){
 				$travelStation = new CarTravelStation();
 				$travelStation->car_travel_id = $carTravel->id;
@@ -162,12 +162,12 @@ class CashierController extends Controller {
 			} else{
 				return response()->json('station_id error',400,['charset'=>'utf-8'],JSON_UNESCAPED_UNICODE);
 			}
-			
-			
-			
+
+
+
 			$car = Car::findOrFail($data['car_id']);
 			$carType = CarType::find($car->car_type_id);
-			
+
 			for ($i = 1 ; $i <= $carType->count_places; $i++){
 				$travelPlace = new CarTravelPlace();
 				$travelPlace->car_travel_id = $carTravel->id;
@@ -177,9 +177,9 @@ class CashierController extends Controller {
 				$travelPlace->to_station_id = $request['to_station_id'];
 				$travelPlace->save();
 			}
-			
-		
-			
+
+
+
 			foreach (json_decode($request['price_places']) as $place_price) {
 				CarTravelPlace::where('car_travel_id',$carTravel->id)
 					->whereBetween('number',[$place_price->from,$place_price->to])
@@ -187,16 +187,16 @@ class CashierController extends Controller {
 						'price' => $place_price->price
 					]);
 			}
-			
+
 			DB::commit();
-			
+
 			return response()->json('success', 200);
 		} catch (\Exception $exception) {
 			DB::rollBack();
 			return response()->json('Server error. Please try again', 500);
 		}
 	}
-	
+
 	public function travelUpcoming(){
         $travels = CarTravel::join('cars','car_travel.car_id','cars.id')
             //->whereRaw("car_travel.destination_time >= CURRENT_TIMESTAMP()")
@@ -207,15 +207,15 @@ class CashierController extends Controller {
             ->get();
         return response()->json(TravelResource::collection($travels),200);
     }
-	
+
 	public function getCarInfo($car_id)
 	{
 		$car = Car::whereId($car_id)
 					->with('car_type')
 					->first();
-		return response()->json($car);			
+		return response()->json($car);
 	}
-	
+
 	public function getTicketsForToday()
 	{
 		$travels = CarTravel::join('cars','car_travel.car_id','cars.id')
@@ -224,10 +224,10 @@ class CashierController extends Controller {
             ->orderBy('car_travel.id','desc')
             ->limit(100)
             ->get();
-			
+
 		return response()->json(TravelResourceForCashier::collection($travels),200,['charset'=>'utf-8'],JSON_UNESCAPED_UNICODE);
 	}
-	
+
 	public function getTicketsForNextDays()
 	{
 		$today = date('Y-m-d');
@@ -237,16 +237,16 @@ class CashierController extends Controller {
             ->orderBy('car_travel.id','desc')
             ->limit(100)
             ->get();
-			
+
 		return response()->json(TravelResourceForCashier::collection($travels),200,['charset'=>'utf-8'],JSON_UNESCAPED_UNICODE);
 	}
-	
+
 	public function getAllPlacesForRoute($car_travel_id)
 	{
 		$car_travel = CarTravel::findOrFail($car_travel_id);
 		return response()->json($car_travel->get_all_places);
 	}
-	
+
 	public function ticketSelling(Request $request, $car_travel_id)
 	{
 		$data = $request->all();
@@ -254,20 +254,20 @@ class CashierController extends Controller {
 		$first_name = $data['first_name'];
 		$phone = str_replace(' ', '', $data['phone']);
 		$iin = $data['iin'];
-		
+
 		if ($this->checkingForDoubleIin($car_travel_id, $iin)) {
 			return response()->json("С таким $iin уже продано билет. Укажите другой ИИН", 409);
 		}
-		
+
 		if ($this->checkingForPhone($car_travel_id, $phone)) {
 			return response()->json("В одном поездке может только 4 раза повторяется телефон. Укажите другой", 409);
 		}
-		
+
 		DB::beginTransaction();
-		
+
 		try {
 			$car_travel = CarTravel::findOrFail($car_travel_id);
-			
+
 			$car_travel_order = new CarTravelOrder();
 			$car_travel_order->car_travel_id = $car_travel->id;
 			$car_travel_order->driver_id = Car::find($car_travel->car_id)->user_id;
@@ -275,27 +275,27 @@ class CashierController extends Controller {
 			$car_travel_order->to_station_id = $car_travel->to_station_id;
 			$car_travel_order->status = 'take';
 			$car_travel_order->save();
-			
-			
+
+
 			$car_travel_place = CarTravelPlace::where('car_travel_id', $car_travel_id)->where('number', $place_number)->first();
-			
+
 			if ($car_travel_place) {
 				if ($car_travel_place->status == 'take') {
                     return response()->json("Место #$place_number уже забронирован", 400);
 
                 }
-                
+
 				if ($car_travel_place->status == 'in_process') {
                     return response()->json("Место #$place_number уже забронирован", 400);
                 }
-				
+
 				$car_travel_place->car_travel_order_id = $car_travel_order->id;
                 $car_travel_place->status = 'take';
                 $car_travel_place->save();
 			} else {
 				return response()->json("Место не найдено", 400);
 			}
-			
+
 			$car_travel_place_order = new CarTravelPlaceOrder();
 			$car_travel_place_order->price = $car_travel_place->price;
 			$car_travel_place_order->car_travel_id = $car_travel_place->car_travel_id;
@@ -309,19 +309,19 @@ class CashierController extends Controller {
 			$car_travel_place_order->iin = $iin;
 			$car_travel_place_order->status = 'take';
 			$car_travel_place_order->save();
-			
+
 			DB::commit();
-			
+
 			return response()->json('Место успешно продано');
-			
+
 		} catch (\Exception $exception) {
 			DB::rollBack();
 			return response()->json('Server error. Please try again', 500);
 		}
 	}
-	
+
 	public function getSoldTicketsForToday($tomorrow = false)
-	{	
+	{
 		if ($tomorrow) {
 			$car_travel_sold_tickes_for_today = CarTravelPlace::where(['car_travel_places.status' => 'take'])
 					->selectRaw('car_travel_places.*, cars.state_number, DATE_FORMAT(car_travel.departure_time, "%d.%m.%Y") as dep_date, DATE_FORMAT(car_travel.departure_time, "%H:%i") as dep_time, DATE_FORMAT(car_travel.destination_time, "%d.%m.%Y") as des_date, DATE_FORMAT(car_travel.destination_time, "%H:%i") as des_time')
@@ -348,11 +348,11 @@ class CashierController extends Controller {
 					->limit(100)
 					->get();
 			//$car_travel_sold_tickes_for_today = $car_travel_sold_tickes_for_today->whereDate("car_travel.departure_time", Carbon::today()->toDateString())->get();
-		}		
-					
-		return response()->json($car_travel_sold_tickes_for_today);	
+		}
+
+		return response()->json($car_travel_sold_tickes_for_today);
 	}
-	
+
 	public function getSoldTickets()
 	{
 		$car_travel_sold_tickes_for_today = CarTravelPlaceOrder::where(['car_travel_place_orders.status' => 'take'])
@@ -365,39 +365,39 @@ class CashierController extends Controller {
 					->orderBy('car_travel.id','desc')
 					->limit(100)
 					->get();
-		
-		return response()->json($car_travel_sold_tickes_for_today);		
+
+		return response()->json($car_travel_sold_tickes_for_today);
 	}
-	
+
 	public function getTicketsByFilter($filter_id)
 	{
 		switch((int)$filter_id) {
 			case 0:
 				return $this->getSoldTicketsForToday();
 			break;
-			
+
 			case 1:
 				return $this->getSoldTicketsForToday(true);
 			break;
-			
+
 			case 2:
 				return $this->getSoldTickets();
 			break;
 		}
 	}
-	
+
 	public function checkingForDoubleIin($car_travel_id, $iin)
 	{
 		$result = CarTravelPlaceOrder::where(['car_travel_id' => $car_travel_id, 'iin' => $iin])->first();
 		return ($result) ? true : false;
 	}
-	
+
 	public function checkingForPhone($car_travel_id, $phone)
 	{
 		$result = CarTravelPlaceOrder::where(['car_travel_id' => $car_travel_id, 'phone' => $phone])->get();
 		return (count($result) >= 4) ? true : false;
 	}
-	
+
 	public function returnSoldTickets(Request $request)
 	{
 		$data = $request->all();
@@ -406,13 +406,13 @@ class CashierController extends Controller {
 			$car_travel_place_order->status = 'cancel';
 			$car_travel_place_order->reason_for_return = $data['reason_for_return'];
 			$car_travel_place_order->save();
-			
+
 			return response()->json("Заявка на возврат принять!", 200);
 		} else {
 			return response()->json("Не найдено место для возврата", 409);
 		}
 	}
-	
+
 	public function getReturnTickets()
 	{
 		$car_travel_return_tickes_for_today = CarTravelPlaceOrder::where(['car_travel_place_orders.status' => 'cancel'])
@@ -426,16 +426,16 @@ class CashierController extends Controller {
 					->orderBy('car_travel.id','desc')
 					->limit(100)
 					->get();
-		return response()->json($car_travel_return_tickes_for_today);		
+		return response()->json($car_travel_return_tickes_for_today);
 	}
-	
+
 	public function getTravelsByFilter(Request $request)
 	{
 		$data = $request->all();
 		$from_city_id = $data['from_city_id'];
 		$to_city_id = $data['to_city_id'];
 		$filter_id = $data['filter_id'];
-		
+
 		if ($filter_id == 0) {
 			$travels = CarTravel::join('cars','car_travel.car_id','cars.id')
 			//->whereDate('car_travel.destination_time', Carbon::today()->toDateString())
@@ -456,18 +456,18 @@ class CashierController extends Controller {
             ->limit(100)
             ->get();
 		}
-		
-		
+
+
         return response()->json(TravelResource::collection($travels),200);
 	}
-	
+
 	public function getInformationAboutCarTravel($car_travel_id)
 	{
 		/*$car_travel = CarTravel::where('id', $car_travel_id)
 					->with('car')
 					->first();
 		return response()->json($car_travel);*/
-		
+
 		$travels = CarTravel::join('cars','car_travel.car_id','cars.id')
             ->whereRaw("car_travel.id = $car_travel_id")
             ->orderBy('car_travel.id','desc')
@@ -475,20 +475,31 @@ class CashierController extends Controller {
             ->get();
         return response()->json(TravelResource::collection($travels),200);
 	}
-	
+
 	public function getSoldTicketsForCurrentRoute($car_travel_id)
 	{
-		$car_travel_sold_tickes = CarTravelPlace::where(['car_travel_places.status' => 'take'])
+		/*$car_travel_sold_tickets = CarTravelPlace::where(['car_travel_places.status' => 'take'])
 					->selectRaw('car_travel_places.*, car_travel_place_orders.first_name,car_travel_place_orders.phone,car_travel_place_orders.iin, cars.state_number, DATE_FORMAT(car_travel.departure_time, "%d.%m.%Y") as dep_date, DATE_FORMAT(car_travel.departure_time, "%H:%i") as dep_time, DATE_FORMAT(car_travel.destination_time, "%d.%m.%Y") as des_date, DATE_FORMAT(car_travel.destination_time, "%H:%i") as des_time')
 					->with('driver', 'from_station', 'to_station')
 					->join('car_travel','car_travel.id','car_travel_places.car_travel_id')
 					->join('cars','car_travel.car_id','cars.id')
 					//->leftJoin('company_cars','company_cars.car_id','cars.id')
 					//->leftJoin('companies', 'companies.id', 'company_cars.company_id')
-					->leftJoin('car_travel_place_orders', 'car_travel_place_orders.car_travel_id', 'car_travel.id')
+					->join('car_travel_place_orders', 'car_travel_place_orders.car_travel_id', 'car_travel_places.car_travel_id')
 					->where(["car_travel.id" => $car_travel_id])
 					->orderBy('car_travel.id','desc')
-					->get();
-		return response()->json($car_travel_sold_tickes);			
+					->get();*/
+
+        $car_travel_sold_tickets = CarTravelPlaceOrder::where(['car_travel_place_orders.car_travel_id' => $car_travel_id, 'car_travel_place_orders.status' => 'take'])
+            ->selectRaw('car_travel_place_orders.*, cars.state_number, DATE_FORMAT(car_travel.departure_time, "%d.%m.%Y") as dep_date, DATE_FORMAT(car_travel.departure_time, "%H:%i") as dep_time, DATE_FORMAT(car_travel.destination_time, "%d.%m.%Y") as des_date, DATE_FORMAT(car_travel.destination_time, "%H:%i") as des_time')
+            ->with('driver', 'from_station', 'to_station')
+            ->join('car_travel','car_travel.id','car_travel_place_orders.car_travel_id')
+            ->join('cars','car_travel.car_id','cars.id')
+            //->leftJoin('company_cars','company_cars.car_id','cars.id')
+            //->leftJoin('companies', 'companies.id', 'company_cars.company_id')
+            ->orderBy('car_travel.id','desc')
+            ->get();
+
+		return response()->json($car_travel_sold_tickets);
 	}
 }
